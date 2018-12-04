c716634f98de6842d8ca0954c4a0198c413069a8
Hylobatide <45590082+Hylobatide@users.noreply.github.com>
2018-12-04 11:28:57 +0100

    Add files via upload

--- Projet/main.cpp
+++ Projet/main.cpp
@@ -1,16 +1,59 @@
 #include <irrlicht.h>
+#include "events.h"
+#include "gui_ids.h"
 
 using namespace irr;
-namespace iv = irr::video;
+
+namespace ic = irr::core;
 namespace is = irr::scene;
+namespace iv = irr::video;
+namespace ig = irr::gui;
+
 
 int main()
 {
+  
+  EventReceiver receiver;
+  std::vector<iv::ITexture*> textures;
+
   // Création de la fenêtre et du système de rendu.
-  IrrlichtDevice *device = createDevice(iv::EDT_OPENGL);
+  IrrlichtDevice *device = createDevice(iv::EDT_OPENGL,
+                                        ic::dimension2d<u32>(1280, 960),
+                                        16, false, false, false, &receiver);
 
   iv::IVideoDriver  *driver = device->getVideoDriver();
   is::ISceneManager *smgr = device->getSceneManager();
+   ig::IGUIEnvironment *gui = device->getGUIEnvironment();
+// Ajout de l’archive qui contient entre autres un niveau complet
+device->getFileSystem()->addFileArchive("data/ikzdm1.pk3");
+//Onchargeunbsp(unniveau)enparticulier:
+is::IAnimatedMesh *scenemesh=smgr->getMesh("ikzdm1.bsp");
+is::ISceneNode *scene;
+scene=smgr->addOctreeSceneNode(scenemesh->getMesh(0),nullptr,-1,1024);
+// Translation pour que nos personnages soient dans le dé cor
+ scene->setPosition ( core :: vector3df ( -1300 , -104 , -1249));
+
+
+   // Chargement de notre personnage
+  is::IAnimatedMesh *mesh = smgr->getMesh("data/tris.md2");
+ // Attachement de notre personnage dans la scène
+  is::IAnimatedMeshSceneNode *node = smgr->addAnimatedMeshSceneNode(mesh);
+  node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+  node->setMD2Animation(irr::scene::EMAT_STAND);
+  textures.push_back(driver->getTexture("data/base.pcx"));
+  textures.push_back(driver->getTexture("data/red_texture.pcx"));
+  textures.push_back(driver->getTexture("data/blue_texture.pcx"));
+  node->setMaterialTexture(0, textures[0]);
+  receiver.set_gui(gui);
+  receiver.set_node(node);
+  receiver.set_textures(textures);
+
+ scene::ICameraSceneNode *camera = smgr->addCameraSceneNodeFPS (0, 100.0f, 0.1f, -1);
+
+
+
+
+
 
   while(device->run())
   {
