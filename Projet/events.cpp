58163604d0c7d6698a03f7d29d56db590626d4ea
Unknown <dav.benveniste@gmail.com>
2018-12-11 10:29:41 +0100

    Score

--- Projet/events.cpp
+++ Projet/events.cpp
@@ -37,198 +37,197 @@ bool EventReceiver::keyboard_handler(const SEvent &event)
 		{
 			case KEY_ESCAPE:
 			exit(0);
-      case KEY_KEY_Z: // Avance
-      position.X += speed * cos(rotation.Y * M_PI / 180.0);
-      position.Z += -speed * sin(rotation.Y * M_PI / 180.0);
-      states[forward]=true;
-      states[stand]=false;
-      break;
-      case KEY_KEY_S: // Recule
-      position.X += -speed * cos(rotation.Y * M_PI / 180.0);
-      position.Z += speed * sin(rotation.Y * M_PI / 180.0);
-      break;
-      case KEY_KEY_D: // Tourne à droite
-      rotation.Y += 10;
-      break;
-      case KEY_KEY_Q: // Tourne à gauche
-      rotation.Y -= 10;
-      break;
-      default:;
+      		case KEY_KEY_Z: // Avance
+      		position.X += speed * cos(rotation.Y * M_PI / 180.0);
+      		position.Z += -speed * sin(rotation.Y * M_PI / 180.0);
+      		states[forward]=true;
+      		states[stand]=false;
+      		break;
+     		case KEY_KEY_S: // Recule
+     		position.X += -speed * cos(rotation.Y * M_PI / 180.0);
+     		position.Z += speed * sin(rotation.Y * M_PI / 180.0);
+     		break;
+      		case KEY_KEY_D: // Tourne à droite
+      		rotation.Y += 10;
+      		break;
+      		case KEY_KEY_Q: // Tourne à gauche
+      		rotation.Y -= 10;
+      		break;
+      		default:;
+      	}
+      	node->setPosition(position);
+      	node->setRotation(rotation);
+      }
+      else
+      {
+      	states[stand]=true;
+      	states[forward]=false;
+      }
+
+
+      if(states[forward]==true)
+      {
+      	if(states[running]==false)
+      	{
+      		node->setMD2Animation(irr::scene::EMAT_RUN);
+      		states[running]=true;
+      		states[stand]=false;
+      	}
+
+      }
+      else
+      {
+      	if(states[stand]==true)
+      	{
+      		node->setMD2Animation(irr::scene::EMAT_STAND);
+      		states[stand]=true;
+      		states[running]=false;
+      	}
+
+      }
+
+
+      return false;
   }
-  node->setPosition(position);
-  node->setRotation(rotation);
-}
-else
-{
-	states[stand]=true;
-	states[forward]=false;
-}
-
-
-if(states[forward]==true)
-{
-	if(states[running]==false)
-	{
-		node->setMD2Animation(irr::scene::EMAT_RUN);
-		states[running]=true;
-		states[stand]=false;
-	}
-
-}
-else
-{
-	if(states[stand]==true)
-	{
-		node->setMD2Animation(irr::scene::EMAT_STAND);
-		states[stand]=true;
-		states[running]=false;
-	}
-
-}
-
-
-return false;
-}
 
 /*------------------------------------------------------------------------*\
  * EventReceiver::mouse_handler                                           *
 \*------------------------------------------------------------------------*/
-bool EventReceiver::mouse_handler(const SEvent &event)
-{
-	switch(event.MouseInput.Event)
-	{
-		case EMIE_LMOUSE_PRESSED_DOWN:
-		button_pressed = true;
-		old_x = event.MouseInput.X;
-		old_y = event.MouseInput.Y;
-		break;
-		case EMIE_LMOUSE_LEFT_UP:
-		button_pressed = false;
-		break;
-		case EMIE_MOUSE_MOVED:
-		if (button_pressed)
-		{
-			ic::vector3df rotation = node->getRotation();
-			rotation.Y -= (event.MouseInput.X - old_x);        
-			old_x = event.MouseInput.X;
-			old_y = event.MouseInput.Y;
-			node->setRotation(rotation);
-		}
-		break;
-		default:
-		;
-	}
+  bool EventReceiver::mouse_handler(const SEvent &event)
+  {
+  	switch(event.MouseInput.Event)
+  	{
+  		case EMIE_LMOUSE_PRESSED_DOWN:
+  		button_pressed = true;
+  		old_x = event.MouseInput.X;
+  		old_y = event.MouseInput.Y;
+  		break;
+  		case EMIE_LMOUSE_LEFT_UP:
+  		button_pressed = false;
+  		break;
+  		case EMIE_MOUSE_MOVED:
+  		if (button_pressed)
+  		{
+  			ic::vector3df rotation = node->getRotation();
+  			rotation.Y += (event.MouseInput.X - old_x);        
+  			old_x = event.MouseInput.X;
+  			old_y = event.MouseInput.Y;
+  			node->setRotation(rotation);
+  		}
+  		break;
+  		default:
+  		;
+  	}
 
-	return false;
-}
+  	return false;
+  }
 
 /*------------------------------------------------------------------------*\
  * EventReceiver::gui_handler                                             *
 \*------------------------------------------------------------------------*/
-bool EventReceiver::gui_handler(const SEvent &event)
-{
-	if (!node) return false;
-	switch(event.GUIEvent.EventType)
-	{
+  bool EventReceiver::gui_handler(const SEvent &event)
+  {
+  	if (!node) return false;
+  	switch(event.GUIEvent.EventType)
+  	{
     // Gestion des menus de la barre de menu
-		case ig::EGET_MENU_ITEM_SELECTED:
-		{
-			ig::IGUIContextMenu *menu = (ig::IGUIContextMenu*)event.GUIEvent.Caller;
-			s32 item = menu->getSelectedItem();
-			s32 id = menu->getItemCommandId(item);
-			u32 debug_info = node->isDebugDataVisible();
+  		case ig::EGET_MENU_ITEM_SELECTED:
+  		{
+  			ig::IGUIContextMenu *menu = (ig::IGUIContextMenu*)event.GUIEvent.Caller;
+  			s32 item = menu->getSelectedItem();
+  			s32 id = menu->getItemCommandId(item);
+  			u32 debug_info = node->isDebugDataVisible();
 
-			switch(id)
-			{
-				case MENU_NEW_GAME:
-				game_on=true;
-				printf("%i\n", game_on);
-				break;
+  			switch(id)
+  			{
+  				case MENU_NEW_GAME:
+  				game_on=true;
+  				break;
 
-				case MENU_QUIT:
-				exit(0);
+  				case MENU_QUIT:
+  				exit(0);
 
-				case MENU_BOUNDING_BOX:
-				menu->setItemChecked(item, !menu->isItemChecked(item));
-				node->setDebugDataVisible(debug_info ^ is::EDS_BBOX);
-				break;
+  				case MENU_BOUNDING_BOX:
+  				menu->setItemChecked(item, !menu->isItemChecked(item));
+  				node->setDebugDataVisible(debug_info ^ is::EDS_BBOX);
+  				break;
 
-				case MENU_NORMALS:
-				menu->setItemChecked(item, !menu->isItemChecked(item));
-				node->setDebugDataVisible(debug_info ^ is::EDS_NORMALS);
-				break;
+  				case MENU_NORMALS:
+  				menu->setItemChecked(item, !menu->isItemChecked(item));
+  				node->setDebugDataVisible(debug_info ^ is::EDS_NORMALS);
+  				break;
 
-				case MENU_TRIANGLES:
-				menu->setItemChecked(item, !menu->isItemChecked(item));
-				node->setDebugDataVisible(debug_info ^ is::EDS_MESH_WIRE_OVERLAY);
-				break;
+  				case MENU_TRIANGLES:
+  				menu->setItemChecked(item, !menu->isItemChecked(item));
+  				node->setDebugDataVisible(debug_info ^ is::EDS_MESH_WIRE_OVERLAY);
+  				break;
 
-				case MENU_TRANSPARENCY:
-				menu->setItemChecked(item, !menu->isItemChecked(item));
-				node->setDebugDataVisible(debug_info ^ is::EDS_HALF_TRANSPARENCY);
-				break;
+  				case MENU_TRANSPARENCY:
+  				menu->setItemChecked(item, !menu->isItemChecked(item));
+  				node->setDebugDataVisible(debug_info ^ is::EDS_HALF_TRANSPARENCY);
+  				break;
 
-				case MENU_ABOUT:
-				break;
-			}
-		}
-		break;
+  				case MENU_ABOUT:
+  				break;
+  			}
+  		}
+  		break;
       // gestion des boutons
-		case ig::EGET_BUTTON_CLICKED:
-		{
-			s32 id = event.GUIEvent.Caller->getID();
-			if (id == MENU_NEW_GAME)
-			{
-				std::cout << "Button clicked\n";
-				game_on=true;
+  		case ig::EGET_BUTTON_CLICKED:
+  		{
+  			s32 id = event.GUIEvent.Caller->getID();
+  			if (id == MENU_NEW_GAME)
+  			{
+  				std::cout << "Button clicked\n";
+  				game_on=true;
 
-			}
-		}
-		break;
+  			}
+  		}
+  		break;
 
-		default:;
-	}
-	return false;
-}
+  		default:;
+  	}
+  	return false;
+  }
 
 /**************************************************************************\
  * EventReceiver::OnEvent                                                 *
 \**************************************************************************/
-bool EventReceiver::OnEvent(const SEvent &event)
-{
-	if (!node) return false;
-	switch (event.EventType)
-	{
-		case EET_KEY_INPUT_EVENT:
-		return keyboard_handler(event);
-		case EET_MOUSE_INPUT_EVENT:
-		return mouse_handler(event);
-		case EET_GUI_EVENT:
-		return gui_handler(event);
-		default:;
-	}
+  bool EventReceiver::OnEvent(const SEvent &event)
+  {
+  	if (!node) return false;
+  	switch (event.EventType)
+  	{
+  		case EET_KEY_INPUT_EVENT:
+  		return keyboard_handler(event);
+  		case EET_MOUSE_INPUT_EVENT:
+  		return mouse_handler(event);
+  		case EET_GUI_EVENT:
+  		return gui_handler(event);
+  		default:;
+  	}
 
-	return false;
-}
+  	return false;
+  }
 
 /**************************************************************************\
  * EventReceiver::set_node                                                *
 \**************************************************************************/
-void EventReceiver::set_node(irr::scene::IAnimatedMeshSceneNode *n)
-{
-	node = n;
-}
+  void EventReceiver::set_node(irr::scene::IAnimatedMeshSceneNode *n)
+  {
+  	node = n;
+  }
 
 /**************************************************************************\
  * EventReceiver::set_gui                                                 *
 \**************************************************************************/
-void EventReceiver::set_gui(irr::gui::IGUIEnvironment *g)
-{
-	gui = g;
-}
+  void EventReceiver::set_gui(irr::gui::IGUIEnvironment *g)
+  {
+  	gui = g;
+  }
 
-bool EventReceiver::getMouvement()
-{
-	return &states;
-}
+  bool EventReceiver::getMouvement()
+  {
+  	return &states;
+  }
 
--- Projet/main.cpp
+++ Projet/main.cpp
@@ -18,7 +18,7 @@ static void create_menu(ig::IGUIEnvironment *gui)
   ig::IGUIContextMenu *menu = gui ->addMenu();
   menu->addItem(L"File", -1 , true, true);
   menu->addItem(L"Debug", -1 , true, true);
-  menu->addItem(L"Help", -1 , true, true);
+  // menu->addItem(L"Help", -1 , true, true);
 
   // Le contenu de menu File
   submenu = menu->getSubMenu(0);
@@ -34,8 +34,8 @@ static void create_menu(ig::IGUIEnvironment *gui)
   submenu->addItem(L"Transparency",   MENU_TRANSPARENCY);
 
   // Le contenu du menu Help :
-  submenu = menu->getSubMenu(2);
-  submenu->addItem(L"About...", MENU_ABOUT);
+  // submenu = menu->getSubMenu(2);
+  // submenu->addItem(L"About...", MENU_ABOUT);
 }
 
 static void create_window(ig::IGUIEnvironment *gui)
@@ -113,8 +113,29 @@ int main()
   ig::IGUIFont* font = gui->getFont("data/fontlucida.png");
   skin->setFont(font);
 
-// La barre de menu
+// Score
+  iv::ITexture *digits[10];
+  digits[0] = driver->getTexture("data/Chiffres/0.png");
+  digits[1] = driver->getTexture("data/Chiffres/1.png");
+  digits[2] = driver->getTexture("data/Chiffres/2.png");
+  digits[3] = driver->getTexture("data/Chiffres/3.png");
+  digits[4] = driver->getTexture("data/Chiffres/4.png");
+  digits[5] = driver->getTexture("data/Chiffres/5.png");
+  digits[6] = driver->getTexture("data/Chiffres/6.png");
+  digits[7] = driver->getTexture("data/Chiffres/7.png");
+  digits[8] = driver->getTexture("data/Chiffres/8.png");
+  digits[9] = driver->getTexture("data/Chiffres/9.png");
 
+  // Création des places pour les chiffres
+  ig::IGUIImage *score_10000 = gui->addImage(ic::rect<s32>(15,15,  55,55)); score_10000->setScaleImage(true);
+  ig::IGUIImage *score_1000  = gui->addImage(ic::rect<s32>(55,15,  95,55)); score_1000->setScaleImage(true);
+  ig::IGUIImage *score_100   = gui->addImage(ic::rect<s32>(95,15,  135,55)); score_100->setScaleImage(true);
+  ig::IGUIImage *score_10    = gui->addImage(ic::rect<s32>(135,15, 175,55)); score_10->setScaleImage(true);
+  ig::IGUIImage *score_1     = gui->addImage(ic::rect<s32>(175,15, 215,55)); score_1->setScaleImage(true);
+
+  int score = 0;
+
+// La barre de menu
   create_menu(gui);
   // create_window(gui);
 
@@ -126,11 +147,22 @@ int main()
     driver->beginScene(true, true, iv::SColor(100,150,200,255));
     gui->drawAll();
 
+        // Calcul du score :
+
+
 
     // Dessin de la scène :
 
     if(receiver.game_on){
       smgr->drawAll();
+      score++; if (score == 50000) score = -1;
+    // Mise à jour du score :
+      score_10000->setImage(digits[(score / 10000) % 10]);
+      score_1000->setImage(digits[(score / 1000) % 10]);
+      score_100->setImage(digits[(score / 100) % 10]);
+      score_10->setImage(digits[(score / 10) % 10]);
+      score_1->setImage(digits[(score / 1) % 10]);
+      gui->drawAll();
     }
     
 
